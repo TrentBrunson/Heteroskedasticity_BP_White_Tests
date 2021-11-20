973699dcca72a6eaa536e8e40e0ea5e8342e4ccc
TrentBrunson <bbiz2@icloud.com>
2021-11-20 11:51:17 -0600

    het test

---
+++ hetTests.py
@@ -0,0 +1,13 @@
+#%%
+# using BP and White Testing for Heteroscedasticity
+# going to use statsmodels for the OLS linear regression
+
+# import dependencies
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import statsmodels.stats.api as sms
+import statsmodels.formula.api as smf
+from statsmodels.compat import lzip
+import matplotlib.pyplot as plt
+# %%
